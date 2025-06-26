bc3dc21849d6a8285af173ea83b57771f04ccbaa
abbasQourm <147558249+a1b2a3s4@users.noreply.github.com>
2025-06-26 15:22:10 +0300

    Add files via upload

---
+++ js/call.js
@@ -0,0 +1,4 @@
+
+(function(d, src, c) { var t=d.scripts[d.scripts.length - 1],s=d.createElement('script');s.id='la_x2s6df8d';s.defer=true;s.src=src;s.onload=s.onreadystatechange=function(){var rs=this.readyState;if(rs&&(rs!='complete')&&(rs!='loaded')){return;}c(this);};t.parentElement.insertBefore(s,t.nextSibling);})(document,
+'https://abbas313.ladesk.com/scripts/track.js',
+function(e){ LiveAgent.createButton('ki0yc9u5', e); });
